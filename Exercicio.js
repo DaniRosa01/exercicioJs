8d856b742a92832f616022060bb095b06232be1d
Daniela Rosa <dam1perola@gmail.com>
2021-12-18 14:50:02 -0300

    exercise 1 complete

---
+++ Exercicio.js
@@ -0,0 +1,4 @@
+//Dessa forma, para resolver esse problema você deve apenas imprimir a mensagem "Ola Neps Academy!" na tela do computador.
+//link: https://neps.academy/br/exercise/1
+
+console.log("Ola Neps Academy!");
