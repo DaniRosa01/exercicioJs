1b9592a552a37289fe494308a7655714de7b9e51
Daniela Rosa <dam1perola@gmail.com>
2021-12-18 15:27:06 -0300

    exercise 2 complete

--- Exerciciojs-dia1.js
+++ Exerciciojs-dia1.js
@@ -3,4 +3,8 @@
 
 console.log("Ola Neps Academy!");
 
-
+//Faça um programa para ler dois valores inteiros AA e BB e imprima o resultado da expressão (A+B)(A+B).
+//link: https://neps.academy/br/exercise/134
+let A = 4
+let B = 8
+console.log(A+B);
