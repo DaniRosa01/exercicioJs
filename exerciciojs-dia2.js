47c2f5e5d68596c6500db4baa535ad39f3655e3e
Daniela Rosa <dam1perola@gmail.com>
2021-12-22 16:15:46 -0300

    second exercise of the day

--- exerciciojs-dia2.js
+++ exerciciojs-dia2.js
@@ -2,3 +2,15 @@
 
 let L = 2;
 console.log(L*L);
+
+//Você deve escrever um programa que, dadas as posições das portinhas PP e RR, neste flíper da figura, diga por qual dos três caminhos, A, B ou C, a bolinha vai cair!
+
+let P = 1;
+let R = 0;
+if(P==1 && R==1) {
+    console.log('A');
+} else if(P==1 && R==0) {
+    console.log('B');
+} else {
+    console.log('C')
+}
