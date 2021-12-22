4b0184cf446d34a5ca0ce0217feece17027c6209
Daniela Rosa <dam1perola@gmail.com>
2021-12-22 16:23:46 -0300

    mudanças, colocando informações do site

--- exerciciojs-dia2.js
+++ exerciciojs-dia2.js
@@ -1,9 +1,10 @@
 //Faça um programa para um inteiro LL e imprima a área de uma quadrado de lado LL.
-
+//link: https://neps.academy/br/exercise/138
 let L = 2;
 console.log(L*L);
 
 //Você deve escrever um programa que, dadas as posições das portinhas PP e RR, neste flíper da figura, diga por qual dos três caminhos, A, B ou C, a bolinha vai cair!
+//link: https://neps.academy/br/exercise/87
 
 let P = 1;
 let R = 0;
